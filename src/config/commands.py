4f4b45066337dce7d285746cece1d6de62474530
dscrypton <108694674+crypton1871@users.noreply.github.com>
2023-01-28 08:47:10 +0100

    Add files via upload

---
+++ src/config/commands.py
@@ -0,0 +1,16 @@
+
+
+
+# Bot commands configuration
+COMMANDS = [
+
+    # Main commands
+    'main.help',
+
+    # RolePlay commands
+    'rp.me',
+    'rp.try',
+    'rp.do',
+    'rp.coinflip',
+
+]
